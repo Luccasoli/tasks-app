35ce92fcc36dbe6674c27b2d9bfaa43ed4539325
l <oliveira.mesquita@aluno.uece.br>
2019-04-19 20:44:13 -0300

    eslit adicionado

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Icon from "react-native-vector-icons/FontAwesome";
-import moment from "moment";
-import "moment/locale/pt-br";
-import commonStyles from "../";
+/* eslint-disable no-undef */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import commonStyles from '../commonStyles';
 
 export default props => {
 	let check = null;
--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
-import moment from "moment";
-import "moment/locale/pt-br";
-import todayImage from "../../assets/imgs/today.jpg";
-import commonStyles from "../commonStyles";
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import todayImage from '../../assets/imgs/today.jpg';
+import commonStyles from '../commonStyles';
 
 export default class Agenda extends Component {
 	render() {
@@ -14,8 +16,8 @@ export default class Agenda extends Component {
 						<Text style={styles.title}>Hoje</Text>
 						<Text style={styles.subtitle}>
 							{moment()
-								.locale("pt-br")
-								.format("ddd, D [de] MMMM")}
+								.locale('pt-br')
+								.format('ddd, D [de] MMMM')}
 						</Text>
 					</View>
 				</ImageBackground>
@@ -38,7 +40,7 @@ const styles = StyleSheet.create({
 	},
 	titleBar: {
 		flex: 1,
-		justifyContent: "flex-end"
+		justifyContent: 'flex-end'
 	},
 	title: {
 		fontFamily: commonStyles.fontFamily,
