61cb63b5b264fa5aa8fa9f50fa4330474c13b9f3
luccasoli <oliveira.mesquita@aluno.uece.br>
2019-05-18 14:32:49 -0300

    Controllers de tasks

--- backend/api/users.js
+++ backend/api/users.js
@@ -1,5 +1,10 @@
 const bcrypt = require("bcrypt-nodejs");
 
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 module.exports = app => {
   const obterHash = (password, callback) => {
     bcrypt.genSalt(10, (err, salt) => {
@@ -8,8 +13,12 @@ module.exports = app => {
       });
     });
   };
-  
+
   const save = (req, res) => {
+    if (!validateEmail(req.body.email)) {
+      res.status(400).send("Insira um email válido");
+    }
+
     obterHash(req.body.password, hash => {
       const password = hash;
 
@@ -21,7 +30,7 @@ module.exports = app => {
           password
         })
         .then(_ => res.status(204).send())
-        .catch(err => res.status(400).json(err));
+        .catch(err => res.status(500).json(err));
     });
   };
 
