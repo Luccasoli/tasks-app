6dcb7d8f86ca838da643486b14c9a416eee067e1
luccasoli <oliveira.mesquita@aluno.uece.br>
2019-05-26 17:33:00 -0300

    criação do drawer navigation

--- mobile/src/Navigator.js
+++ mobile/src/Navigator.js
@@ -1,6 +1,59 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import React from 'react';
+import {
+	createAppContainer,
+	createSwitchNavigator,
+	createDrawerNavigator
+} from 'react-navigation';
 import Agenda from './screens/Agenda';
 import Auth from './screens/Auth';
+import commonStyles from './commonStyles';
+
+const MenuRoutes = {
+	Today: {
+		name: 'today',
+		screen: props => <Agenda title="Hoje" daysAhead={0} {...props} />,
+		navigationOptions: {
+			title: 'Hoje'
+		}
+	},
+	Tomorrow: {
+		name: 'tomorrow',
+		screen: props => <Agenda title="Amanhã" daysAhead={1} {...props} />,
+		navigationOptions: {
+			title: 'Amanhã'
+		}
+	},
+	Week: {
+		name: 'week',
+		screen: props => <Agenda title="Semana" daysAhead={7} {...props} />,
+		navigationOptions: {
+			title: 'Semana'
+		}
+	},
+	Month: {
+		name: 'month',
+		screen: props => <Agenda title="Mês" daysAhead={30} {...props} />,
+		navigationOptions: {
+			title: 'Mês'
+		}
+	}
+};
+
+const MenuConfig = {
+	initialRouteName: 'Today',
+	contentOptions: {
+		labelStyle: {
+			fontFamily: commonStyles.fontFamily,
+			fontWeight: 'normal',
+			fontSize: 20
+		},
+		activeLabelStyle: {
+			color: '#080'
+		}
+	}
+};
+
+const MenuNavigator = createDrawerNavigator(MenuRoutes, MenuConfig);
 
 const MainRoutes = {
 	Auth: {
@@ -9,7 +62,7 @@ const MainRoutes = {
 	},
 	Home: {
 		name: 'Home',
-		screen: Agenda
+		screen: MenuNavigator
 	}
 };
 
