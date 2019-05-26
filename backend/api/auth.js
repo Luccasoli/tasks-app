d65a8bc384b04f2505037a7a8465ad6f3c5c50c8
luccasoli <oliveira.mesquita@aluno.uece.br>
2019-05-26 19:04:19 -0300

    correçao no case sensitive do email

--- backend/api/auth.js
+++ backend/api/auth.js
@@ -3,38 +3,38 @@ const jwt = require("jwt-simple");
 const bcrypt = require("bcrypt-nodejs");
 
 module.exports = app => {
-    /*
+	/*
         req.body: {
             email, password
         }
     */
-    const signin = async (req, res) => {
-        if (!req.body.email || !req.body.password) {
-            return res.status(400).send("Dados incompletos");
-        }
+	const signin = async (req, res) => {
+		if (!req.body.email || !req.body.password) {
+			return res.status(400).send("Dados incompletos");
+		}
 
-        const user = await app
-            .db("users")
-            .where({ email: req.body.email })
-            .first();
+		const user = await app
+			.db("users")
+			.whereRaw("LOWER(email) = LOWER(?)", req.body.email)
+			.first();
 
-        if (user) {
-            bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
-                if (err || !isMatch) {
-                    return res.status(401).send();
-                }
+		if (user) {
+			bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+				if (err || !isMatch) {
+					return res.status(401).send();
+				}
 
-                const payload = { id: user.id };
-                res.json({
-                    name: user.name,
-                    email: user.email,
-                    token: jwt.encode(payload, authSecrets)
-                });
-            });
-        } else {
-            return res.status(400).send("Usuário não cadastrado");
-        }
-    };
+				const payload = { id: user.id };
+				res.json({
+					name: user.name,
+					email: user.email,
+					token: jwt.encode(payload, authSecrets)
+				});
+			});
+		} else {
+			return res.status(400).send("Usuário não cadastrado");
+		}
+	};
 
-    return { signin };
+	return { signin };
 };
